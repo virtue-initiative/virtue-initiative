b8f08eace4af309f46097930cb3f2c7fb9a5ca6a
Anbcodes <code@anb.codes>
2026-02-23 16:55:10 -0500

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd $(dirname $0) > /dev/null
+
+pushd ./api > /dev/null && npm run deploy
+popd && pushd ./web > /dev/null && npm run deploy
+popd && pushd ./landing > /dev/null && npx wrangler deploy
+popd && pushd ./help > /dev/null && npm run deploy
+
+popd
+
