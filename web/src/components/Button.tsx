2827c34fc75e1e1463f5e71c86f037fce259dd08
Anbcodes <code@anb.codes>
2026-03-03 15:56:17 -0500

    Added a button comment that supports an icon

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,16 @@
+export function Button({ children, onClick, className, icon }: { children: preact.VNode; onClick: () => void; className?: string; icon?: preact.VNode }) {
+  if (icon) {
+    return (
+      <button class={`btn text-icon ${className}`} onClick={onClick} type="button">
+        <div class="icon">{icon}</div>
+        <div class="text">{children}</div>
+      </button>
+    );
+  } else {
+    return (
+      <button class={`btn ${className}`} onClick={onClick} type="button">
+        {children}
+      </button>
+    );
+  }
+}
