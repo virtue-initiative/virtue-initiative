2890f0ec9287ccc59273d24ca269e6d44c526fd6
Jeffrey Baumes <jeffbaumes@gmail.com>
2026-03-15 17:22:39 -0400

    Remove astro server

--- landing/src/theme.ts
+++
@@ -1,5 +0,0 @@
-export const THEME_COOKIE_NAME = "virtue-theme";
-
-export function normalizeThemeCookie(theme: string | undefined) {
-  return theme === "dark" || theme === "light" ? theme : undefined;
-}
