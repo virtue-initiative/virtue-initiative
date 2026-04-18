a94ce84ab114483d805fc010cc8e547eec4dbbfd
Anbcodes <code@anb.codes>
2026-04-18 17:05:29 -0400

    Fixed prettier

--- api/src/index.ts
+++ api/src/index.ts
@@ -22,7 +22,9 @@ app.use(
   '/*',
   cors({
     origin: (origin, c) => {
-      const allowedOrigins = [c.env.APP_URL, 'http://localhost:5173'].map((url) => new URL(url).origin);
+      const allowedOrigins = [c.env.APP_URL, 'http://localhost:5173'].map(
+        (url) => new URL(url).origin,
+      );
       return allowedOrigins.find((o) => o === origin);
     },
     allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
