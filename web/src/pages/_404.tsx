e0f0c2940d5b7433834b420fb2d439a51b3964f0
anbcodes <code@anb.codes>
2026-05-13 10:54:14 -0400

    Added nicer 404 pages (#357)

--- web/src/pages/_404.tsx
+++ web/src/pages/_404.tsx
@@ -1,8 +1,21 @@
+import "./404.css";
+
 export function NotFound() {
   return (
-    <section>
-      <h1>404: Not Found</h1>
-      <p>It's gone :(</p>
-    </section>
+    <div class="not-found">
+      <h1 class="not-found-title">Page not found</h1>
+      <p class="not-found-message">
+        Sorry, we couldn't find the page you were looking for. It may have been
+        moved or deleted.
+      </p>
+      <div class="not-found-actions">
+        <a href="/" class="btn btn-primary">
+          Go to dashboard
+        </a>
+        <a href="/settings" class="btn btn-ghost">
+          Settings
+        </a>
+      </div>
+    </div>
   );
 }
